39a994245f80f9ede53617fbb67584598f9f0e94
Kylianna <kyliemisiak@gmail.com>
2025-01-31 14:19:33 -0800

    Program Outline

---
+++ calculator.c
@@ -0,0 +1,20 @@
+
+
+// File: CS 480 Lab 2 Calculator
+// Author: Kylianna Misiak
+// Source code file
+
+/*  Calculator application with ability to do basic arithmetic/trig/
+logarithmic operations w/ real numbers and "()" and "{}"
+*/
+
+// No external software used besides Math library
+
+
+// "-" used as unary and binary operator
+
+/* Mathematic expression evaluation implemented in code, do not use eval
+ or internal eval functions
+*/
+
+// Software should check for correct inputs
